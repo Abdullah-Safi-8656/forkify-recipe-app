ae6e49a03911e5bfd23724f71e93d5f7d3f46051
Abdullah-Safi-8656 <safiabdullah769@example.com>
2026-08-28 08:55:36 -0700

    fetch recipe details from API

--- src/js/controller.js
+++ src/js/controller.js
@@ -12,3 +12,35 @@ const timeout = function (s) {
 // https://forkify-api.jonas.io
 
 ///////////////////////////////////////
+const url =
+  'https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e82b4';
+
+const showRecipe = async function () {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.log(response);
+    if (!response.ok)
+      throw new Error(`something went wrong please try again later`);
+
+    console.log(data);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(recipe);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+showRecipe();
