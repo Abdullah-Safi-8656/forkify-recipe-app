f62b457a8ae4ab8f7bd4481de92e96deeb67881c
Abdullah-Safi-8656 <safiabdullah769@example.com>
2026-08-30 01:09:00 -0700

    refactor: split application into modules using MVC architecture
    - Separated model, view, and controller logic
    - Organized recipe data and UI rendering into dedicated modules
    - Improved code structure, readability, and maintainability
    - Added Fraction.js for formatting ingredient quantities

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,8 @@
-import icons from 'url:../img/icons.svg';
+import * as modle from './model.js';
+import recipeViwe from './RecipeView/recipeViwer.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-
-const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -18,158 +17,29 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
-const renderSpiner = function (ParentEl) {
-  const markup = `
-    <div class="spinner">
-      <svg>
-        <use href="${icons}#icon-loader"></use>
-      </svg>
-    </div> 
-  `;
-  ParentEl.innerHTML = '';
-  ParentEl.insertAdjacentHTML('afterbegin', markup)
 
-};
 
-const showRecipe = async function () {
+const controllerRecipe = async function () {
   try {
-
     const id = window.location.hash.slice(1);
-    console.log(id)
+    console.log(id);
 
-    if (!id) {
-      return 
-    };
+    if (!id) return;
+    recipeViwe.renderSpiner();
 
-    // 1) Loading recipe and loading spinner.
-    renderSpiner(recipeContainer)
-    const response = await fetch(
-      `https://forkify-api.jonas.io/api/v2/recipes/${id}`,
-    );
-
-    const data = await response.json();
-
-    if (!response.ok)
-      throw new Error(`something went wrong please try again later`);
-
-    console.log(data);
-
-    let { recipe } = data.data;
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-    console.log(recipe);
+    // 1) Loading recipe.
+    await modle.loadRecipe(id);
+    const { recipe } = modle.state;
 
     // Rendering recipe
+    recipeViwe.render(modle.state.recipe);
 
-    const markup = `
-
-      <figure class="recipe__fig">
-          <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img" />
-          <h1 class="recipe__title">
-            <span>${recipe.title}</span>
-          </h1>
-        </figure>
-
-        <div class="recipe__details">
-          <div class="recipe__info">
-            <svg class="recipe__info-icon">
-              <use href="${icons}#icon-clock"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
-            <span class="recipe__info-text">minutes</span>
-          </div>
-          <div class="recipe__info">
-            <svg class="recipe__info-icon">
-              <use href="${icons}#icon-users"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
-            <span class="recipe__info-text">servings</span>
-
-            <div class="recipe__info-buttons">
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use href="${icons}#icon-minus-circle"></use>
-                </svg>
-              </button>
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use href="${icons}#icon-plus-circle"></use>
-                </svg>
-              </button>
-            </div>
-          </div>
-
-          <div class="recipe__user-generated">
-            <svg>
-              <use href="${icons}#icon-user"></use>
-            </svg>
-          </div>
-          <button class="btn--round">
-            <svg class="">
-              <use href="${icons}#icon-bookmark-fill"></use>
-            </svg>
-          </button>
-        </div>
-
-        <div class="recipe__ingredients">
-          <h2 class="heading--2">Recipe ingredients</h2>
-          <ul class="recipe__ingredient-list">
-          ${recipe.ingredients
-            .map(ing => {
-              return `
-             <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="src/img/icons.svg#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">${ing.quantity}</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">${ing.unit}</span>
-                ${ing.description}
-              </div>
-            </li>
-
-            `;
-            })
-            .join('')}
-          
-          </ul>
-        </div>
-
-        <div class="recipe__directions">
-          <h2 class="heading--2">How to cook it</h2>
-          <p class="recipe__directions-text">
-            This recipe was carefully designed and tested by
-            <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
-            directions at their website.
-          </p>
-          <a
-            class="btn--small recipe__btn"
-            href="${recipe.sourceUrl}"
-            target="_blank"
-          >
-            <span>Directions</span>
-            <svg class="search__icon">
-              <use href="src/img/icons.svg#icon-arrow-right"></use>
-            </svg>
-          </a>
-        </div>
-    `;
-    recipeContainer.innerHTML = '';
-    recipeContainer.insertAdjacentHTML('afterbegin', markup);
   } catch (err) {
-    recipeContainer.innerHTML = ''
+    recipeContainer.innerHTML = '';
     alert(err);
   }
 };
 
 // showRecipe();
 
-['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controllerRecipe));
